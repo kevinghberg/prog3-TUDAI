2dde20fb9a8de3c490b1ac6f3e3f9951e2497c8d
KOKOPC <kkydota@gmail.com>
2024-08-27 20:42:26 -0300

    estaEnPosicion() implementado punto 6 tp2

--- src/main/java/tp2/Main.java
+++ src/main/java/tp2/Main.java
@@ -3,13 +3,15 @@ package tp2;
 public class Main {
     public static void main(String[] args) {
 
-        int[] arr1 = {1,2,3,4,5};
+        int[] arr1 = {1, 2, 3, 4, 5};
 
         int[] arr2 = {1, 4, 2, 5, 3};
 
+        int[] arr3 = {-3, -1, 0, 2, 4, 6, 10};
+
         System.out.println(estaOrdenado(arr2));
 
-        System.out.println(buscarElemento(3,arr1,0,arr1.length-1));
+        System.out.println(buscarElemento(3, arr1, 0, arr1.length - 1));
 
         System.out.println(convertirABinario(26));
 
@@ -18,45 +20,70 @@ public class Main {
     }
 
     private static boolean estaOrdenadoRecursivo(int[] arr, int tamanio) {
-        if (tamanio >= arr.length-1) {
+        if (tamanio >= arr.length - 1) {
             return true;
-        } else if (arr[tamanio] > arr[tamanio+1]) {
+        } else if (arr[tamanio] > arr[tamanio + 1]) {
             return false;
         }
-        return estaOrdenadoRecursivo(arr,tamanio+1);
+        return estaOrdenadoRecursivo(arr, tamanio + 1);
     }
 
-     public static boolean estaOrdenado(int[] arr) {
-        return estaOrdenadoRecursivo(arr,0);
+    public static boolean estaOrdenado(int[] arr) {
+        return estaOrdenadoRecursivo(arr, 0);
     }
 
-    public static int buscarElemento(int elem, int[] arr, int inicio, int fin){
-        int medio=0;
-        if(fin<inicio)
+    public static int buscarElemento(int elem, int[] arr, int inicio, int fin) {
+        int medio = 0;
+        if (fin < inicio)
             return -1;
         else
-            medio = (inicio+fin)/2;
-        if(elem>arr[medio]){
-            return buscarElemento(elem,arr,medio+1,fin);
+            medio = (inicio + fin) / 2;
+        if (elem > arr[medio]) {
+            return buscarElemento(elem, arr, medio + 1, fin);
         } else if (elem < medio) {
-            return buscarElemento(elem,arr,inicio,medio-1);
-        }else
+            return buscarElemento(elem, arr, inicio, medio - 1);
+        } else
             return medio;
     }
 
-    public static String convertirABinario(int i){
-        if(i == 0){
+    public static String convertirABinario(int i) {
+        if (i == 0) {
             return "0";
-        }if(i == 1){
+        }
+        if (i == 1) {
             return "1";
-        }else
-            return convertirABinario(i/2)+(i%2);
+        } else
+            return convertirABinario(i / 2) + (i % 2);
     }
 
-    private static int calcularFibonacci(int n){
-        if(n <= 1){
+    private static int calcularFibonacci(int n) {
+        if (n <= 1) {
             return n;
         }
-        return calcularFibonacci(n-1) + calcularFibonacci(n-2);
+        return calcularFibonacci(n - 1) + calcularFibonacci(n - 2);
+    }
+
+    private static boolean estaEnPosicion(int[] arr, int i) {
+        if (i >= arr.length - 1) {
+            return false;
+        } else if (i == arr[i]) {
+            return true;
+        }
+        return estaEnPosicion(arr, i + 1);
+    }
+
+    public void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+
+            mergeSort(array, left, mid);
+
+
+            mergeSort(array, mid + 1, right);
+
+
+            //merge(array, left, mid, right);
+        }
     }
 }
