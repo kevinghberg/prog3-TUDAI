f5179ecaa579ceb3425d80284ea5aa9db77bebe2
KOKOPC <kkydota@gmail.com>
2024-08-30 00:05:13 -0300

    mergesort

--- src/main/java/tp2/Main.java
+++ src/main/java/tp2/Main.java
@@ -17,6 +17,34 @@ public class Main {
 
         System.out.println(calcularFibonacci(6));
 
+
+        // ordenamiento seleccion
+        int[] arr = {64, 25, 12, 22, 11};
+        ordenamientoPorSeleccion(arr);
+        System.out.println("\n"+"Arreglo ordenado por selección:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        //ordenamiento burbujeo
+        int[] arrSe = {64, 34, 25, 12, 22, 11, 90};
+        ordenamientoPorBurbuja(arrSe);
+        System.out.println("\n"+"Arreglo ordenado por burbuja:");
+        for (int num : arrSe) {
+            System.out.print(num + " ");
+        }
+
+
+        // mergesort
+
+        int[] arrMS = {12, 11, 13, 5, 6, 7};
+        System.out.println("\n"+"Arreglo original:");
+        printArray(arrMS);
+
+        mergeSort(arrMS, 0, arrMS.length - 1);
+
+        System.out.println("Arreglo ordenado por Merge Sort:");
+        printArray(arrMS);
     }
 
     private static boolean estaOrdenadoRecursivo(int[] arr, int tamanio) {
@@ -72,18 +100,117 @@ public class Main {
         return estaEnPosicion(arr, i + 1);
     }
 
-    public void mergeSort(int[] array, int left, int right) {
-        if (left < right) {
-            int mid = (left + right) / 2;
+    // Merge Sort es muy eficiente y tiene una complejidad de  O(nlog n) en todos los casos,
+    // lo que lo hace adecuado para ordenar grandes conjuntos de datos.
+
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            // Encuentra el punto medio
+            int m = (l + r) / 2;
+
+            // Ordena la primera y la segunda mitad recursivamente
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+
+            // Mezcla las mitades ordenadas
+            merge(arr, l, m, r);
+        }
+    }
+
+    public static void merge(int[] arr, int l, int m, int r) {
+        // Encuentra los tamaños de los subarreglos para ser mezclados
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Arrays temporales para almacenar los subarreglos
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copia los datos a los arrays temporales L[] y R[]
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[m + 1 + j];
+        }
+
+        // Índices iniciales de los primeros y segundos subarreglos
+        int i = 0, j = 0;
+
+        // Índice inicial del arreglo mezclado
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copia los elementos restantes de L[], si los hay
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copia los elementos restantes de R[], si los hay
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 
 
-            mergeSort(array, left, mid);
 
 
-            mergeSort(array, mid + 1, right);
+    //  Este algoritmo tiene una complejidad de  O(n elevado 2 ) en todos los
+    //  porque siempre realiza n(n−1)/2 comparaciones independientemente de la organización inicial de los elementos.
+    public static void ordenamientoPorSeleccion(int[] arr) {
 
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            // Intercambiar el mínimo elemento encontrado con el primer elemento
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
 
-            //merge(array, left, mid, right);
+        }
+
+    }
+
+    // El algoritmo tiene una complejidad de 𝑂 ( n elevado 2 ) en el caso promedio y peor, pero puede alcanzar  O(n)
+    // si se introduce una optimización para detener el algoritmo cuando no se realicen intercambios durante una pasada (esto indica que la lista ya está ordenada).
+    public static void ordenamientoPorBurbuja(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Intercambiar los elementos si están en el orden incorrecto
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // Si no hubo intercambios, la lista ya está ordenada
+            if (!swapped) break;
         }
     }
 }
